52d153c111b9eac085a2cf2fc54529908897d06e
Breno Galdino <brenogaldino21@gmail.com>
2023-03-03 18:15:35 -0300

    first commit

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,33 @@
+import Cronometro from './Cronometro.vue';
+
+<template>
+  <Caixa>
+    <div class="columns">
+      <div class="column is-7">{{tarefa.descricao || 'Simple Task'}}</div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
+    </div>
+  </Caixa>
+</template>
+
+<script lang="ts">
+import ITarefa from "../interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
+import Cronometro from "./Cronometro.vue";
+import Caixa from "./Caixa.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: {
+    Cronometro,
+    Caixa
+},
+  props: {
+    tarefa: {
+        type: Object as PropType<ITarefa>,
+        required: true
+    }
+  }
+});
+</script>
